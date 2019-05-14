abd7a91d268b9055a276135ea8637b12d709de26
Adrian <Adri98.1mm@gmail.com>
2019-05-14 17:26:15 -0600

    creacion clase controlador

---
+++ controller.ts
@@ -0,0 +1,10 @@
+export class controller {
+    private audioS1: any;
+    private audioS2: any;
+
+    public constructor(pAudioS1: any, pAudioS2: any) {
+        this.audioS1 = pAudioS1;
+        this.audioS2 = pAudioS2;
+    }
+
+}
--- wav-test.ts
+++ wav-test.ts
@@ -25,29 +25,7 @@ const readFile = (filepath: string) => {
 readFile("./S1/IDragons.wav").then((buffer) => {
   return WavDecoder.decode(buffer);
 }).then(function(audioData) {
-  console.log("ampliando 30%");
-  const size = 20000;
-
-  for(var i=0; i<10; i++) {
-    console.log(audioData.channelData[0][i]);
-    console.log(audioData.channelData[1][i]);
-    console.log('*******************');
-  }
-
-  // for(var i=0; i<audioData.channelData[0].length; i++) {
-  //   audioData.channelData[1][i]+=audioData.channelData[0][i];
-  //   audioData.channelData[0][i]*=20;
-  //   audioData.channelData[0][i]+=0.000000259254;
-  // }
-
-  for(var i=44100*5; i<44100*10; i++) {
-    audioData.channelData[0][i-44100*5] = audioData.channelData[0][i];
-  }
-
-  for(var i=44100*11; i<44100*16; i++) {
-    audioData.channelData[0][i+44100*6] = audioData.channelData[0][i];
-  }
-
+  
 
   console.log("writing...");
   WavEncoder.encode(audioData).then((buffer: any) => {
